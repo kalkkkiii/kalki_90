da25a945bb910bb987f4ff1793d1ef6cd0724b9c
kalkkkiii <147042371+kalkkkiii@users.noreply.github.com>
2023-10-05 14:38:39 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,28 @@
+#1.1 implement a recursive function to calculate the factorial of a given number.
+
+"""
+1! = 1 Ã— 1
+2! = 2 Ã— 1! ---> 2 Ã— 1
+3! = 3 Ã— 2! ---> 3 Ã— 2 Ã— 1
+.
+.
+10! = 10 Ã— 9!
+
+formula - n Ã— (n-1)!
+"""
+
+
+def fact_rec(n):
+  if n==0 or n==1:
+     return 1
+  else:
+    return n*fact_rec(n-1)
+  if n==0 or n==1:
+    return 1
+  else:
+    return n * fact_rec(n-1)
+    
+number = int (input ("enter a value : "))
+res = fact_rec(number)
+
+print ("the factorial of {} is {}.".format (number,res))
---
+++ main (7).py
@@ -0,0 +1,25 @@
+"""Implement a class called Player that represents a cricket player. The Player class should have a 
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and 
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman 
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.""" 
+
+# define a base class player
+class player:
+  def play(self):
+      print("the player is playing cricket")
+# define the Batsman class,derived from player
+class Batsman(player):
+  def play(self):
+      print("Batsman is batting")
+# define  the Bowler class, derived from player
+class Bowler(player):
+  def play(self):
+      print("Bowler is bowling")
+# create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# call the play() method for each object
+batsman.play()
+bowler.play()
